ee299e2c3d7c286f8af9394d8ec0904c5717de57
ghost2050 <gonzalolopezguerrero2050@gmail.com>
2020-06-10 03:33:59 -0500

    se creo nuevanotadialogviewmodel

--- app/src/main/java/com/example/uifirst/NuevaNotaDialogViewModel.java
+++ app/src/main/java/com/example/uifirst/NuevaNotaDialogViewModel.java
@@ -1,7 +1,33 @@
 package com.example.uifirst;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NuevaNotaDialogViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import com.example.uifirst.db.entity.NotaEntity;
+
+import java.util.List;
+
+public class NuevaNotaDialogViewModel extends AndroidViewModel {
+    private LiveData<List<NotaEntity>> allNotas; // lista q se actualiza
+    private NotaRepositoty notaRepositoty; // clase facilita los datos de room o api
+
+    public NuevaNotaDialogViewModel(Application application) {
+        super(application);
+        notaRepositoty = new NotaRepositoty(application);
+        allNotas = notaRepositoty.getAll();
+    }
+
+    //frgamento q necesita recibir la nueva lista de datos
+    public LiveData<List<NotaEntity>> getAllNotas() {
+        return allNotas;
+    }
+
+    //frgamento q insert una nueva nota
+    public void insrtNota(NotaEntity nuevaNotaEntity) {
+        notaRepositoty.insert(nuevaNotaEntity);
+    }
+
 }
